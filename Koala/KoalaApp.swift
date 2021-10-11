f27c60bb37f3a1828e13df02008d9b20fb26e15d
revarinoo <revarinoputra@gmail.com>
2021-10-11 10:40:30 +0700

    Setup project

---
+++ Koala/KoalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoalaApp.swift
+//  Koala
+//
+//  Created by Revarino Putra on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct KoalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
