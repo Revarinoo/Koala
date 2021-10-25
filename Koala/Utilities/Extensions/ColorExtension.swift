c981088193c70fb1d63c5471c8de250fe9399b38
JonathanClive <2201jota@gmail.com>
2021-10-25 11:52:10 +0700

    Revision and several fix in ColorExtension

--- Koala/Utilities/Extensions/ColorExtension.swift
+++ Koala/Utilities/Extensions/ColorExtension.swift
@@ -11,7 +11,16 @@ extension Color {
     public static var bgColorView: Color {
         return Color("background")
     }
-<<<<<<< HEAD
+    
+    public static var orange1: Color {
+        return Color("orange1")
+    }
+    public static var orange2: Color {
+        return Color("orange2")
+    }
+    public static var checkbox: Color {
+        return Color("checkbox")
+    }
     
     init(hex: String) {
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
@@ -36,15 +45,5 @@ extension Color {
             blue:  Double(b) / 255,
             opacity: Double(a) / 255
         )
-=======
-    public static var orange1: Color {
-        return Color("orange1")
-    }
-    public static var orange2: Color {
-        return Color("orange2")
-    }
-    public static var checkbox: Color {
-        return Color("checkbox")
->>>>>>> main
     }
 }
