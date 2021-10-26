a078b55c2607dab02da9b967d61b8fec6888c713
revarinoo <revarinoputra@gmail.com>
2021-10-26 20:04:14 +0700

    Modify Followers Profile

--- View/ChildView/FollowerProfile.swift
+++ View/ChildView/FollowerProfile.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct FollowerProfile: View {
     
     @State var currentIndex: Int = 0
-    @State var data: [FollowersData] = []
+    let images = ["offer","offer","ig"]
     
     var body: some View {
         VStack (alignment: .leading) {
@@ -19,14 +19,9 @@ struct FollowerProfile: View {
                 .padding(.leading, 16)
                 .padding(.bottom, 16)
             VStack (alignment: .center, spacing: 14) {
-                ImageSlider(index: $currentIndex, items: data) { profile in
-                    Image(profile.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(10)
-                }
+                ImageSlider(index: $currentIndex, images: images)
                 HStack {
-                    ForEach(data.indices, id:\.self) { index in
+                    ForEach(images.indices, id:\.self) { index in
                         Circle()
                             .fill(currentIndex == index ? ThemeColor.primary: Color.init(hex: "C4C4C4"))
                             .frame(width: 10, height: 10)
@@ -34,11 +29,6 @@ struct FollowerProfile: View {
                             .animation(.spring(), value: currentIndex == index)
                     }
                 }
-            }
-        }
-        .onAppear {
-            for _ in 1...5 {
-                data.append(FollowersData(image: "offer"))
             }
         }
         
@@ -49,9 +39,4 @@ struct FollowerProfile_Previews: PreviewProvider {
     static var previews: some View {
         FollowerProfile()
     }
-}
-
-struct FollowersData: Identifiable {
-    var id = UUID().uuidString
-    var image: String
 }
--- View/ChildView/ImageSlider.swift
+++ View/ChildView/ImageSlider.swift
@@ -7,54 +7,28 @@
 
 import SwiftUI
 
-struct ImageSlider<Content: View, T: Identifiable>: View {
+struct ImageSlider: View {
     
-    
-    var content: (T) -> Content
-    var list: [T]
-    
-    var spacing: CGFloat
-    var trailingSpace: CGFloat
     @Binding var index: Int
     
     @GestureState var offset: CGFloat = 0
     @State var currentIndex: Int = 0
     
-    init(spacing: CGFloat = 15, trailingSpace: CGFloat = 100, index: Binding<Int>, items: [T], @ViewBuilder content: @escaping (T)-> Content) {
-        self.list = items
-        self.spacing = spacing
-        self.trailingSpace = trailingSpace
-        self._index = index
-        self.content = content
-    }
+    var images: [String] = []
     
     var body: some View {
-//        VStack {
-//            TabView {
-//                ForEach(images, id: \.self) { item in
-//                    Image(item)
-//                        .resizable()
-//                        .scaledToFill()
-//                        .frame(width: 284, height: 218, alignment: .center)
-//                }
-//            }
-//            .tabViewStyle(PageTabViewStyle())
-//        }
-//        .frame(width: 358, height: 218)
-//        .background(Color.white)
-//        .cornerRadius(10)
-//        .shadow(color: Color.gray, radius: 3, x: 0, y: 3)
-        
         
         GeometryReader { proxy in
             
             let width = proxy.size.width
             
             HStack (spacing: 0) {
-                ForEach(list) { item in
+                ForEach(images, id: \.self) { item in
                     VStack {
-                        content(item)
-                            .frame(width: width)
+                        Image(item)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(10)
                     }
                     .frame(width: 358, height: 218)
                     .background(Color.white)
@@ -74,7 +48,7 @@ struct ImageSlider<Content: View, T: Identifiable>: View {
                         let offsetX = value.translation.width
                         let progress = -offsetX / width
                         let roundIndex = progress.rounded()
-                        currentIndex = max(min(currentIndex + Int(roundIndex), list.count-1), 0)
+                        currentIndex = max(min(currentIndex + Int(roundIndex), images.count-1), 0)
                         
                         currentIndex = index
                     })
@@ -82,7 +56,7 @@ struct ImageSlider<Content: View, T: Identifiable>: View {
                         let offsetX = value.translation.width
                         let progress = -offsetX / width
                         let roundIndex = progress.rounded()
-                        index = max(min(currentIndex + Int(roundIndex), list.count-1), 0)
+                        index = max(min(currentIndex + Int(roundIndex), images.count-1), 0)
                     })
             )
         }
