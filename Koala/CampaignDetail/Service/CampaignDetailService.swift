79d6d1c831294537f776fcb2894f9572efcec632
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-11-03 10:02:18 +0700

    Fix header auth service campaign-details

--- Koala/CampaignDetail/Service/CampaignDetailService.swift
+++ Koala/CampaignDetail/Service/CampaignDetailService.swift
@@ -6,13 +6,14 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct CampaignDetailService {
-    
+    @AppStorage("JWT", store: .standard) var token = ""
     func getCampaignDetail(_ id: Int, completionHandler:@escaping(_ result: CampaignDetailResponse?)->Void) {
         
         let request = NSMutableURLRequest(url: NSURL(string: "http://127.0.0.1:8000/api/campaign/detail/\(id)")! as URL)
-        request.allHTTPHeaderFields = HttpUtility.shared.headers
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
 
         HttpUtility.shared.request(request as URLRequest, resultType: CampaignDetailResponse.self) { response in
             _ = completionHandler(response)
