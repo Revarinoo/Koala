72af46cd7d51fe70cdc7f9a6a3cd5290262090d8
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-11-15 09:24:24 +0700

    Fix alert after review success or not

--- Koala/Review/View/ReviewInfluencerView.swift
+++ Koala/Review/View/ReviewInfluencerView.swift
@@ -15,29 +15,30 @@ struct ReviewInfluencerView: View {
     var name: String
     var orderId: Int
     
-//    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    //    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @ObservedObject var reviewVM = ReviewPageViewModel()
     @State var uiTabarController: UITabBarController?
+    @State var submitButtonPressed: Bool = false
     
     var body: some View {
         ZStack {
             Color.bgColorView.edgesIgnoringSafeArea(.all)
             
-//            VStack {
-//                HStack {
-//                    Button(action: {
-//
-//                    }, label: {
-//                        Image(systemName: "chevron.left")
-//                            .font(Font.custom(ThemeFont.poppinsMedium, size: 20))
-//                            .foregroundColor(ThemeColor.primary)
-//                            .cornerRadius(10)
-//                    })
-//                        .padding(EdgeInsets(top: 0, leading: 10, bottom: 8, trailing: 0))
-//                    Spacer()
-//                }
-//                Spacer()
-//            }
+            //            VStack {
+            //                HStack {
+            //                    Button(action: {
+            //
+            //                    }, label: {
+            //                        Image(systemName: "chevron.left")
+            //                            .font(Font.custom(ThemeFont.poppinsMedium, size: 20))
+            //                            .foregroundColor(ThemeColor.primary)
+            //                            .cornerRadius(10)
+            //                    })
+            //                        .padding(EdgeInsets(top: 0, leading: 10, bottom: 8, trailing: 0))
+            //                    Spacer()
+            //                }
+            //                Spacer()
+            //            }
             
             VStack(alignment: .center) {
                 WebImage(url: URL(string: photoURL))
@@ -80,36 +81,32 @@ struct ReviewInfluencerView: View {
                             .stroke(.white, lineWidth: 10))
                 .padding(.horizontal, 20)
                 
-                NavigationLink(
-                    destination: Text("Review Success").navigationBarBackButtonHidden(true),
-                    isActive: $reviewVM.reviewModel.navigate,
-                    label: {
-                        Button {
-                            if(reviewVM.validateUserReview()) {
-                                reviewVM.submitReview(orderId: orderId)
-                            }
-                        } label: {
-                            Text("Submit")
-                            .padding(15)
-                            .font(Font.custom(ThemeFont.poppinsSemiBold, size: 18))
-                            .frame(minWidth: 326, maxWidth: .infinity, alignment: .center)
-                        }
-                        .foregroundColor(.white)
-                        .background(ThemeColor.primary)
-                        .cornerRadius(12)
-                        .padding(.bottom, 8)
-                        .alert(isPresented: $reviewVM.reviewModel.isPresentingErrorAlert, content: {
-                            Alert(title: Text("Alert"), message: Text(reviewVM.reviewModel.errorMessage), dismissButton: .cancel(Text("Ok")))
-                        })
-                    })
-                    .padding(EdgeInsets(top: 48, leading: 16, bottom: -24, trailing: 16))
+                Button {
+                    if(reviewVM.validateUserReview()) {
+                        reviewVM.submitReview(orderId: orderId)
+                        submitButtonPressed.toggle()
+                    }
+                } label: {
+                    Text("Submit")
+                        .padding(15)
+                        .font(Font.custom(ThemeFont.poppinsSemiBold, size: 18))
+                        .frame(minWidth: 326, maxWidth: .infinity, alignment: .center)
+                }
+                .foregroundColor(.white)
+                .background(ThemeColor.primary)
+                .cornerRadius(12)
+                .padding(.bottom, 8)
+                .alert(isPresented: $submitButtonPressed, content: {
+                    Alert(title: reviewVM.reviewModel.isPresentingErrorAlert ? Text("Alert") : Text("Success"), message: reviewVM.reviewModel.isPresentingErrorAlert ? Text(reviewVM.reviewModel.errorMessage) : Text("Review already recorded, Thank you"), dismissButton: .cancel(Text("Ok")))
+                })
+                .padding(EdgeInsets(top: 48, leading: 16, bottom: -24, trailing: 16))
             }
             .introspectTabBarController { (UITabBarController) in
-                                    UITabBarController.tabBar.isHidden = true
-                                    uiTabarController = UITabBarController
-                                }.onDisappear{
-                                    uiTabarController?.tabBar.isHidden = false
-                                }
+                UITabBarController.tabBar.isHidden = true
+                uiTabarController = UITabBarController
+            }.onDisappear{
+                uiTabarController?.tabBar.isHidden = false
+            }
         }
         .onTapGesture {
             self.dismissKeyboard()
