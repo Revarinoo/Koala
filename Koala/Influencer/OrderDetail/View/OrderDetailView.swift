aad22d3600c31903289d5dd3464632474a977c61
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-11-23 21:26:30 +0700

    Add backbone folder

---
+++ Koala/Influencer/OrderDetail/View/OrderDetailView.swift
@@ -0,0 +1,22 @@
+//
+//  OrderDetailView.swift
+//  Koala
+//
+//  Created by Syahrul Apple Developer BINUS on 23/11/21.
+//
+
+import SwiftUI
+
+struct OrderDetailView: View {
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            
+        }
+    }
+}
+
+struct OrderDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderDetailView()
+    }
+}
