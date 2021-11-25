80891f977e71aa409a9067f3c6e91b3da9ea8f56
revarinoo <revarinoputra@gmail.com>
2021-11-25 16:58:19 +0700

    Update review status

--- Koala/Influencer/Order Review/View Model/OrderReviewViewModel.swift
+++ Koala/Influencer/Order Review/View Model/OrderReviewViewModel.swift
@@ -10,16 +10,20 @@ import Foundation
 class OrderReviewViewModel: ObservableObject {
     @Published var data: OrderReviewResponse = OrderReviewResponse(code: 0, message: "", review: ReviewData(comment: "", rating: 0), business: BusinessData(photo: "", name: ""))
     private let service = OrderReviewService()
+    @Published var isReviewed = false
     
     
     func getReview(orderId: Int) {
-        print("HELLO")
         service.getOrderReview(orderId: orderId) { result in
             if let response = result {
                 if response.code == 201 {
                     DispatchQueue.main.async {
                         self.data = OrderReviewResponse(code: response.code, message: response.message, review: response.review, business: response.business)
+                        self.isReviewed = true
                     }
+                }
+                else {
+                    self.isReviewed = false
                 }
             }
         }
--- Koala/Influencer/Order Review/View/OrderReviewView.swift
+++ Koala/Influencer/Order Review/View/OrderReviewView.swift
@@ -18,51 +18,59 @@ struct OrderReviewView: View {
     }
     
     var body: some View {
-        VStack (alignment: .leading) {
-            HStack {
-                WebImage(url: URL(string: orderReviewVM.data.business.photo))
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 78, height: 78)
-                    .cornerRadius(20)
-                VStack (alignment: .leading) {
-                    Text(orderReviewVM.data.business.name)
-                        .font(Font.custom(ThemeFont.poppinsSemiBold, size: 20))
-                        .padding(.bottom, -8)
-                    HStack{
-                        ForEach(1...5, id: \.self) { index in
-                            Image(systemName: self.starType(index: index))
-                                .foregroundColor(.orange1)
-                                .frame(width: 31, height: 29)
-                                .padding(.leading, -7)
+        
+        if orderReviewVM.isReviewed {
+            VStack (alignment: .leading) {
+                HStack {
+                    WebImage(url: URL(string: orderReviewVM.data.business.photo))
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 78, height: 78)
+                        .cornerRadius(20)
+                    VStack (alignment: .leading) {
+                        Text(orderReviewVM.data.business.name)
+                            .font(Font.custom(ThemeFont.poppinsSemiBold, size: 20))
+                            .padding(.bottom, -8)
+                        HStack{
+                            ForEach(1...5, id: \.self) { index in
+                                Image(systemName: self.starType(index: index))
+                                    .foregroundColor(.orange1)
+                                    .frame(width: 31, height: 29)
+                                    .padding(.leading, -7)
+                            }
                         }
                     }
+                    .padding(.leading, 5)
+                    Spacer()
                 }
-                .padding(.leading, 5)
+                Text("Comment")
+                    .font(Font.custom(ThemeFont.poppinsSemiBold, size: 16))
+                    .padding(.top)
+                ZStack (alignment: .topLeading){
+                    Rectangle()
+                        .frame(width: 358, height: 178)
+                        .cornerRadius(10)
+                        .foregroundColor(Color.init(hex: "F3F4F8"))
+                        .shadow(color: Color.init(hex: "BFBFBF"), radius: 3, x: 1, y: 2)
+                    Text(orderReviewVM.data.review.comment)
+                        .font(Font.custom(ThemeFont.poppinsRegular, size: 14))
+                        .padding()
+                }
+                .padding(.top, -10)
                 Spacer()
             }
-            Text("Comment")
-                .font(Font.custom(ThemeFont.poppinsSemiBold, size: 16))
-                .padding(.top)
-            ZStack (alignment: .topLeading){
-                Rectangle()
-                    .frame(width: 358, height: 178)
-                    .cornerRadius(10)
-                    .foregroundColor(Color.init(hex: "F3F4F8"))
-                    .shadow(color: Color.init(hex: "BFBFBF"), radius: 3, x: 1, y: 2)
-                Text(orderReviewVM.data.review.comment)
-                    .font(Font.custom(ThemeFont.poppinsRegular, size: 14))
-                    .padding()
+            .padding()
+            .background(Color.bgColorView.ignoresSafeArea())
+            .navigationTitle("Review")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                orderReviewVM.getReview(orderId: orderId)
             }
-            .padding(.top, -10)
-            Spacer()
         }
-        .padding()
-        .background(Color.bgColorView.ignoresSafeArea())
-        .navigationTitle("Review")
-        .navigationBarTitleDisplayMode(.inline)
-        .onAppear {
-            orderReviewVM.getReview(orderId: orderId)
+        else {
+            Text("This Order Haven't Reviewed Yet")
+                .foregroundColor(ThemeColor.primary)
+                .font(.custom(ThemeFont.poppinsSemiBold, size: 18))
         }
     }
 }
