e321b5c4349245e7b49009f369e8ba75592ecfbe
dholigum <syahrulfadholi@mail.ugm.ac.id>
2021-11-25 07:04:53 +0700

    Add button accept/decline and fix app logo

--- Koala/Influencer/OrderDetail/View/InfluencerCampaignDetailView.swift
+++ Koala/Influencer/OrderDetail/View/InfluencerCampaignDetailView.swift
@@ -13,49 +13,89 @@ struct InfluencerCampaignDetailView: View {
     
     var body: some View {
         ZStack(alignment: .bottomLeading) {
-            VStack {
-                ScrollView(.vertical, showsIndicators: false) {
-                    ZStack(alignment: .top) {
-                        VStack {
-                            InfluencerCampaignDetailFieldView(campaign: campaignVM.campaignModel, campaignDetail: campaignVM.campaignDetailModel)
-                        }
-                        .frame(width: UIScreen.main.bounds.width - 28, alignment: .leading)
-                        .padding(EdgeInsets(top: 60, leading: 28, bottom: 100, trailing: 0))
-                        .background(Color.white)
-                        .cornerRadius(20, corners: [.topLeft, .topRight])
-                    }
+            VStack{
+                Rectangle().fill(ThemeColor.primary)
+                    .frame(height: 800).padding(.top,0).ignoresSafeArea(.all)
+                Spacer()
+            }
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    InfluencerCampaignDetailFieldView(campaign: campaignVM.campaignModel, campaignDetail: campaignVM.campaignDetailModel)
                 }
+                .frame(width: UIScreen.main.bounds.width - 28, alignment: .leading)
+                .padding(EdgeInsets(top: 60, leading: 28, bottom: 28, trailing: 0))
+                .background(Color.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
                 
-                
-                HStack {
-                    WebImage(url: URL(string: campaignVM.campaignModel.business_photo))
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 50, height: 50)
-                        .cornerRadius(20.0)
-                        .overlay(RoundedRectangle(cornerRadius: 20.0)
-                            .stroke(Color.white, lineWidth: 5))
-                    Text(campaignVM.campaignModel.business_name)
-                        .font(Font.custom(ThemeFont.poppinsMedium, size: 16))
+                VStack {
+                    Button {
+                        
+                    } label: {
+                        Text("Accept")
+                            .padding(15)
+                            .font(Font.custom(ThemeFont.poppinsSemiBold, size: 18))
+                            .frame(minWidth: 326, maxWidth: .infinity, alignment: .center)
+                            .foregroundColor(.white)
+                            .background(ThemeColor.primary)
+                            .cornerRadius(15)
+                    }
                     
+                    .padding(.bottom, 8)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Decline")
+                            .padding(15)
+                            .font(Font.custom(ThemeFont.poppinsSemiBold, size: 18))
+                            .frame(minWidth: 326, maxWidth: .infinity, alignment: .center)
+                            .foregroundColor(ThemeColor.primary)
+                            .cornerRadius(15)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(ThemeColor.primary, lineWidth: 1)
+                            )
+                    }
+                    
+                    .padding(.bottom, 120)
+                }
+                .padding(.horizontal, 28)
+            }
+            
+            HStack {
+                WebImage(url: URL(string: campaignVM.campaignModel.business_photo))
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(10)
+                    .shadow(color: ThemeColor.grayDark.opacity(0.4), radius: 3, x: 0, y: 5)
+                    .padding(.trailing, 8)
+                
+                Text(campaignVM.campaignModel.business_name)
+                    .font(Font.custom(ThemeFont.poppinsMedium, size: 16))
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
                     Text("Chat")
                         .font(Font.custom(ThemeFont.poppinsSemiBold, size: 15))
                         .foregroundColor(.white)
-                        .padding(EdgeInsets(top: 4, leading: 10, bottom: 4, trailing: 10))
+                        .padding(EdgeInsets(top: 8, leading: 28, bottom: 8, trailing: 28))
                         .background(ThemeColor.primary)
                         .cornerRadius(10)
                 }
                 
-                .frame(width: UIScreen.main.bounds.width, height: 96)
-                .background(Color(hex: "F2F2F2"))
             }
+            .padding(EdgeInsets(top: -24, leading: 28, bottom: 0, trailing: 28))
+            .frame(width: UIScreen.main.bounds.width, height: 96)
+            .background(Color(hex: "F2F2F2"))
             
         }
         .edgesIgnoringSafeArea(.bottom)
         .onAppear {
             campaignVM.getOrderDetails(id: self.id)
-            print(campaignVM.campaignDetailModel)
-            print("id ye \(id)")
         }
         .introspectTabBarController { (UITabBarController) in
             UITabBarController.tabBar.isHidden = true
